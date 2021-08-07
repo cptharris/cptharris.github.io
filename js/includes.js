d30a0a3a409269d449994cf732ef7f47200c2dd5
TechieCable <67209324+TechieCable@users.noreply.github.com>
2021-08-06 17:53:38 -0700

    Improve scrolling to hashes; Not supporting unencrypted links

--- js/includes.js
+++ js/includes.js
@@ -3,11 +3,18 @@ $(document).ready(function(){
 		$(this).load('includes/' + $(this).attr('src'));
 	})
 	
+	if ($(window.location.hash).length) {
+		scrollTo(window.location.hash);
+	}
+	$(window).on('hashchange', function() {
+		scrollTo(window.location.hash);
+	});
+});
+
+function scrollTo(elem) {
 	// jQuery Scroll Addon
 	// https://gist.github.com/iamravenous/f06b341dbb8b0839a7b0
-	if ($(window.location.hash).length) {
-		$('html, body').animate({
-			scrollTop: $(window.location.hash).offset().top - 100
-		}, 1, 'swing');
-	}
-});
+	$('html, body').animate({
+		scrollTop: $(elem).offset().top * 0.90
+	}, 1, 'swing');
+}
